100f6cea53e8e52f13a36d61e661eec661f7c495
ba <ba@3dsafety.com.au>
2017-08-06 10:54:08 +1000

    fix build warnings

--- src/DaisyParser/Daisy202Parser/Daisy202Parser.fs
+++ src/DaisyParser/Daisy202Parser/Daisy202Parser.fs
@@ -3,11 +3,11 @@ namespace DaisyParser.Parser.Daisy202
 open FParsec
 open FSharp.Data
 open DaisyParser.Parser.Common.Common
+
 module HeadParsers =
   let headParser<'a> = 
     BetweenTags "head"
-  
-  
+    
   // let headParser' =
   //   headParser <| manyChars
     
